2c12fe844b5b50cf03d281ecd57d78af9918e4ae
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-04-26 19:50:28 +0600

    categories added

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
-            <h1>LeftNav</h1>
+            <h4>All Category: {categories.length}</h4>
+            {
+                categories.map(category=><p key={category.id}>
+                    <Link to={`/category/${category.id}`} style={{color: 'black', textDecoration: 'none'}}>{category.name}</Link></p>)
+            }
         </div>
     );
 };
